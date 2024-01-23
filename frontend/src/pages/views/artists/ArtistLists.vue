6c5dbd2472f75d4d409c9f49281539203473c730
trust musikiri <49308670+trustwebmaster@users.noreply.github.com>
2024-01-23 11:53:36 +0200

    bug

--- frontend/src/pages/views/artists/ArtistLists.vue
+++ frontend/src/pages/views/artists/ArtistLists.vue
@@ -1,7 +1,7 @@
 <template>
   <PageComponent>
     <form @submit="searchArtists">
-      <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search For Most Popular Artists</label>
       <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
@@ -12,12 +12,12 @@
         </div>
         <input
           type="search" id="search"
-          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search for Artists..."
+          class="block min-w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search For Most Popular Artists."
           v-model="search">
         <button type="submit"
                 @click="toggle = !toggle"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="text-white absolute right-2.5 bottom-2.5 bg-purple-100 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-800 font-medium rounded-lg text-sm px-4 py-2 dark:bg-purple-800 dark:hover:bg-blue-700 dark:focus:ring-purple-800">
           Search
         </button>
       </div>
